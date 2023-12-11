51ef7b4c3074d28ff3825adb02f418fe4b9b619d
Alvin <2068446872@qq.com>
2023-12-11 11:38:01 +0800

    提交01

---
+++ src/main/java/com/alvin/demo/Actviti7DemoApplication.java
@@ -0,0 +1,13 @@
+package com.alvin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actviti7DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actviti7DemoApplication.class, args);
+	}
+
+}
